3247ff814b5cd4902578e17ae8914a7878e7b446
Lakshya Agrawal <lakshya18jul.vns@gmail.com>
2021-08-28 08:44:01 +0530

    basix express server written in  index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const port = 8000;
+
+const app = express();
+
+app.listen(port, function(err){
+    if(err){
+        console.log("error encountered", err);
+        return;
+    }
+    console.log("yup my express server is working ");
+});
